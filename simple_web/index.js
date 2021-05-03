b8b2caa7ef0e589997ebdf283346541a8b91b53c
Scott Fitzgerald <sfitzgerald@eero.com>
2021-05-03 12:04:06 -0500

    finish project

--- simple_web/index.js
+++ simple_web/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const app = express();
 
-port = 8889
+port = 8080
 
 app.get('/', (req, res) => {
-    res.send('Hi there');
+    res.send('Bye there');
 });
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
